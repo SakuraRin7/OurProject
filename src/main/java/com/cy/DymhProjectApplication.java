75faa4b018fa4c67e5b0ab4b0c79d82581ecbac7
SakuraRin07 <474564335@qq.com>
2019-11-13 15:34:14 +0800

    建库

---
+++ src/main/java/com/cy/DymhProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DymhProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DymhProjectApplication.class, args);
+	}
+
+}
